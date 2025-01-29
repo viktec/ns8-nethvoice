372205cfc462865ee79c0d97a5fbde9484263021
viktec <48328088+viktec@users.noreply.github.com>
2025-01-29 16:49:13 +0100

    Update 50_asterisk.rest_pjsip_trunks_custom_flags.sql
    Add Provider Vianova

--- mariadb/docker-entrypoint-initdb.d/50_asterisk.rest_pjsip_trunks_custom_flags.sql
+++ mariadb/docker-entrypoint-initdb.d/50_asterisk.rest_pjsip_trunks_custom_flags.sql
@@ -30,6 +30,7 @@ INSERT INTO `rest_pjsip_trunks_custom_flags` (`provider_id`,`keyword`,`value`) V
 (25,"disable_topos_header",0),
 (26,"disable_topos_header",0),
 (27,"disable_topos_header",0),
+(28,"disable_topos_header",0),
 (1,"disable_srtp_header",1),
 (2,"disable_srtp_header",1),
 (3,"disable_srtp_header",0),
@@ -56,4 +57,5 @@ INSERT INTO `rest_pjsip_trunks_custom_flags` (`provider_id`,`keyword`,`value`) V
 (24,"disable_srtp_header",1),
 (25,"disable_srtp_header",1),
 (26,"disable_srtp_header",1),
-(27,"disable_srtp_header",1);
+(27,"disable_srtp_header",1),
+(28,"disable_srtp_header",1);
